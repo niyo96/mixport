e18b46deb652cd2485cdecc1af97102ed6246e72
ninowagner <nino.wagner@hotmail.de>
2025-11-20 22:57:21 +0100

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    output: "export", // This might be needed for static export
+    trailingSlash: true,
+    images: {
+        unoptimized: true, // Required for Cloudflare deployment
+    },
+}
 
-export default nextConfig;
+export default nextConfig
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
+import {initOpenNextCloudflareForDev} from "@opennextjs/cloudflare"
+
+initOpenNextCloudflareForDev()
